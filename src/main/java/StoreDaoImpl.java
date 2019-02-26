a6e9959ed376c96febc4105c979e6805995fe196
Konrad <konradsusel@gmail.com>
2019-02-26 11:03:08 +0100

    Add file StoreDaoImpl.java

---
+++ src/main/java/StoreDaoImpl.java
@@ -0,0 +1,32 @@
+package main.java;
+
+public class StoreDaoImpl {
+
+    public void removeItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public String getItems() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public Item getItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public void updateItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public void addItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public void addBasicItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+
+    public void addMagicItem() {
+        throw new UnsupportedOperationException("method not implemented yet");
+    }
+}
