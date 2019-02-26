65355b11b8f5721b14a3dd9296300dc71af1caeb
Konrad <konradsusel@gmail.com>
2019-02-26 10:22:33 +0100

    Add new file Item.java

---
+++ src/main/java/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    String name;
+    String description;
+    int price;
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void editPrice() {
+
+    }
+
+    public String itemToString() {
+        return this.description;
+    }
+}
