d009a89752546b14b14b7fe6033946da9f2a70a9
ejaworska <ewel.jaworska@gmail.com>
2019-03-12 16:36:32 +0100

    add LoginDao

---
+++ src/main/java/dao/LoginDAOImpl.java
@@ -0,0 +1,83 @@
+package dao;
+
+import dao.connectionPool.JDBCConnectionPool;
+import dao.interfaces.LoginDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAOImpl implements LoginDAO {
+    private JDBCConnectionPool connectionPool;
+    private Connection connection = null;
+    private PreparedStatement pst = null;
+
+    public LoginDAOImpl(JDBCConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public boolean isActiveSession(String sessionid) {
+        System.out.println(sessionid);
+        try {
+            connection = connectionPool.takeOut();
+            connection.setAutoCommit(false);
+            pst = connection.prepareStatement("SELECT * FROM session WHERE sessionid = ?");
+            pst.setString(1, sessionid);
+            ResultSet recordFromDatabase = pst.executeQuery();
+            if (recordFromDatabase.next()) {
+                return true;
+            }
+            connection.commit();
+        } catch(SQLException se) {
+            se.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            connectionPool.takeIn(connection);
+        }
+        return false;
+    }
+
+    public int getUserId(String sessionid) {
+        int id = 0;
+        try {
+            connection = connectionPool.takeOut();
+            connection.setAutoCommit(false);
+            pst = connection.prepareStatement("SELECT userid FROM session WHERE sessionid = ?");
+            pst.setString(1, sessionid);
+            ResultSet recordFromDatabase = pst.executeQuery();
+            if (recordFromDatabase.next()) {
+                id = recordFromDatabase.getInt("userid");
+                return id;
+            }
+            connection.commit();
+        } catch(SQLException se) {
+            se.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            connectionPool.takeIn(connection);
+        }
+        return id;
+    }
+
+    public void activateSessionId(String sessionid, int id) {
+        try {
+            connection = connectionPool.takeOut();
+            connection.setAutoCommit(false);
+            pst = connection.prepareStatement("INSERT INTO session (sessionid, userid) VALUES (?, ?)");
+            pst.setString(1, sessionid);
+            pst.setInt(2, id);
+            pst.executeUpdate();
+            System.out.println("session saved: " + sessionid);
+            connection.commit();
+        } catch(SQLException se) {
+            se.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            connectionPool.takeIn(connection);
+        }
+    }
+}
