8562de07802841e6bf539b9232f8110ab12aafdb
ejaworska <ewel.jaworska@gmail.com>
2019-03-12 16:37:12 +0100

    Add Login Dao class

---
+++ src/main/java/dao/interfaces/LoginDAO.java
@@ -0,0 +1,7 @@
+package dao.interfaces;
+
+public interface LoginDAO {
+    boolean isActiveSession(String sessionid);
+    int getUserId(String sessionid);
+    void activateSessionId(String sessionid, int id);
+}
